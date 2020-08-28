c29cb6ce752bf4d7e4be88ba85ecd8a4b79efee1
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2020-08-28 07:37:54 +0000

    Started adding authentication functionality

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const JSONProvider = require('../DataProvider/JSONProvider').JSONProvider;
 const app = express();
+const pass = require('passport');
+const ls = require('passport-local').Strategy;
 const port = 80;
+
+app.use(helmet());
 
 app.listen(port, () => {
     console.log(`Example app listening at http://secret.covrt.co:${port}`);
@@ -17,3 +21,11 @@ app.get('/home', (req, res) => {
     res.send(data);
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile('./Web/login.html');
+});
+
+app.post('/login', pass.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
