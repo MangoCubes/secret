6056133248a377083ac137d1e0620488a3befe39
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2020-09-05 06:50:38 +0000

    Planning Postgre support

---
+++ App/DataProvider/PostgreProvider.js
@@ -0,0 +1,6 @@
+const DataProviderBase = require('./DataProvider.js').DataProviderBase;
+const config = require('../data/config/config.js').configData;
+
+class PostgreProvider extends DataProviderBase{
+    
+}
