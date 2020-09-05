84f16c287619674b60f13a984b178fd103fb061a
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2020-09-05 07:09:38 +0000

    Starting logger object

--- App/Logger.js
+++ App/Logger.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
+
 class Logger{
-    constructor() {
+    constructor(dir) { //dir is relative location of the log file
+        this.dir = dir;
+        this.logfile = fs.createWriteStream(__dirname + '/' + this.dir + '/logs.log', {flags : 'a+'});
+        console.log(this.logfile);
+    }
+
+    connection(ip){
+        this.logfile.write(`INFO: New connection from ${ip}`);
     }
 }
 
