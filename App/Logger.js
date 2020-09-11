99521e91f5aa8708b296875d2c4b1cc23a39d5b0
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2020-09-11 09:09:55 +0000

    Logger generates files properly now

--- App/Logger.js
+++ App/Logger.js
@@ -5,7 +5,7 @@ class Logger{
         this.dir = dir;
         if(!fs.existsSync(this.dir)) fs.mkdirSync(this.dir, { recursive: true }); //If log directory does not exist, create one
         this.globallog = fs.createWriteStream(__dirname + '/' + this.dir + '/logs.log', {flags : 'a+'}); //Create file write stream for global(and anonymous) log
-        this.userlog = __dirname + '/' + this.dir + '/Users';
+        this.userlog = __dirname + '/' + this.dir + '/Users/';
         if(!fs.existsSync(this.userlog)) fs.mkdirSync(this.userlog, { recursive: true }); //If the directory to user specific log does not exist, create one
     }
 
@@ -14,36 +14,40 @@ class Logger{
         this.globallog.write(`${now}: [INFO] New connection from ${ip}.`);
     }
 
-    loginfailed(username, attempts, ip){
-        let file = __dirname + '/' + this.dir + '/Users/' + username + 'login.log';
+    loginfailed(userid, attempts, ip){
+        if(!fs.existsSync(this.userlog + userid)) fs.mkdirSync(this.userlog + userid, { recursive: true });
+        let file = this.userlog + userid + '/login.log';
         let now = new Date();
         let str = fs.createWriteStream(file, {flags : 'a+'});
-        this.str.write(`${now}: [WARNING] Someone from ${ip} tried to log in and failed ${attempts} times in total.`);
+        str.write(`${now}: [WARNING] Someone from ${ip} tried to log in and failed ${attempts} times in total.`);
     }
 
-    loginsuccessful(username, ip){
-        let file = __dirname + '/' + this.dir + '/Users/' + username + 'login.log';
+    loginsuccessful(userid, ip){
+        if(!fs.existsSync(this.userlog + userid)) fs.mkdirSync(this.userlog + userid, { recursive: true });
+        let file = this.userlog + userid + '/login.log';
         let now = new Date();
         let str = fs.createWriteStream(file, {flags : 'a+'});
-        this.str.write(`${now}: [INFO] User logged in from ${ip}.`);
+        str.write(`${now}: [INFO] User logged in from ${ip}.`);
     }
 
-    access(username, dataid, fieldidarr){
-        let file = __dirname + '/' + this.dir + '/Users/' + username + 'login.log';
+    access(userid, dataid, fieldidarr){
+        if(!fs.existsSync(this.userlog + userid)) fs.mkdirSync(this.userlog + userid, { recursive: true });
+        let file = this.userlog + userid + '/login.log';
         let now = new Date();
         let str = fs.createWriteStream(file, {flags : 'a+'});
-        this.str.write(`${now}: [INFO] User accessed data with an ID ${data}. Fields with ID ${fieldidarr.join(', ')} were visible.`);
+        str.write(`${now}: [INFO] User accessed data with an ID ${data}. Fields with ID ${fieldidarr.join(', ')} were visible.`);
     }
 
-    modification(username, dataid, before, after){
+    modification(userid, dataid, before, after){
         
     }
 
-    revelation(username, dataid, fieldid){
-        let file = __dirname + '/' + this.dir + '/Users/' + username + 'login.log';
+    revelation(userid, dataid, fieldid){
+        if(!fs.existsSync(this.userlog + userid)) fs.mkdirSync(this.userlog + userid, { recursive: true });
+        let file = this.userlog + userid + '/login.log';
         let now = new Date();
         let str = fs.createWriteStream(file, {flags : 'a+'});
-        this.str.write(`${now}: [INFO] User revealed hidden field with ID ${fieldid} from data ${dataid}.`);
+        str.write(`${now}: [INFO] User revealed hidden field with ID ${fieldid} from data ${dataid}.`);
     }
 }
 
