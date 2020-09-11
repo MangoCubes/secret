ef85b9523507573d02860034e3a3bc054bea3887
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2020-09-11 09:46:19 +0000

    JSON files are now generated properly

--- App/DataProvider/JSONProvider.js
+++ App/DataProvider/JSONProvider.js
@@ -1,5 +1,6 @@
 //Storing data as JSON is purely for testing purpose. Do not use it for production.
 const DataProviderBase = require('./DataProviderBase').DataProviderBase;
+const fs = require('fs');
 
 class JSONProvider extends DataProviderBase{
     setData(data, key){
@@ -14,24 +15,45 @@ class JSONProvider extends DataProviderBase{
         this._fields[key] = fields;
     }
 
-    setUsers(users, key){
-        this._users[key] = users;
+    setUsers(users){
+        this._users = users;
     }
 
     setStructure(structure, key){
         this._structure[key] = structure;
     }
 
-    constructor(){
+    checkMainDir(dir){
+        if(!fs.existsSync(`${dir}`)) fs.mkdirSync(`${dir}`, { recursive: true });
+        if(!fs.existsSync(`${dir}/users.json`)) fs.writeFileSync(`${dir}/users.json`, '{}', {flag: 'wx'});
+        if(!fs.existsSync(`${dir}/data/`)) fs.mkdirSync(`${dir}/data/`, { recursive: true });
+    }
+
+    checkFile(dir, key){
+        if(!fs.existsSync(`${dir}/data/${key}`)) fs.mkdirSync(`${dir}/data/${key}`, { recursive: true });
+        if(!fs.existsSync(`${dir}/data/${key}/fields.json`)) fs.writeFileSync(`${dir}/data/${key}/fields.json`, '{}', {flag: 'wx'});
+        if(!fs.existsSync(`${dir}/data/${key}/templates.json`)) fs.writeFileSync(`${dir}/data/${key}/templates.json`, '{}', {flag: 'wx'});
+        if(!fs.existsSync(`${dir}/data/${key}/data.json`)) fs.writeFileSync(`${dir}/data/${key}/data.json`, '{}', {flag: 'wx'});
+        if(!fs.existsSync(`${dir}/data/${key}/structure.json`)) fs.writeFileSync(`${dir}/data/${key}/structure.json`, '{}', {flag: 'wx'});
+    }
+
+    constructor(dir){
         super();
-        this.setUsers(require(`../../data/Test/users.json`));
-        Object.keys(this._users).forEach(function(key) {
-            this.setFields(require(`../../data/Test/data/${key}/fields.json`), key);
-            this.setTemplates(require(`../../data/Test/data/${key}/templates.json`), key);
-            this.setData(require(`../../data/Test/data/${key}/data.json`), key);
-            this.setStructure(require(`../../data/Test/data/${key}/structure.json`), key);
-        });
-        
+        dir = __dirname + '/' + dir;
+        this._data = {};
+        this._templates = {};
+        this._fields = {};
+        this._structure = {};
+        this.checkMainDir(dir);
+        this.setUsers(require(`${dir}/users.json`));
+        let arr = Object.keys(this._users);
+        for(let i = 0; i < arr.length; i++){
+            this.checkFile(dir, arr[i]);
+            this.setFields(JSON.parse(fs.readFileSync(`${dir}/data/${arr[i]}/fields.json`)), arr[i]);
+            this.setTemplates(JSON.parse(fs.readFileSync(`${dir}/data/${arr[i]}/templates.json`)), arr[i]);
+            this.setData(JSON.parse(fs.readFileSync(`${dir}/data/${arr[i]}/data.json`)), arr[i]);
+            this.setStructure(JSON.parse(fs.readFileSync(`${dir}/data/${arr[i]}/structure.json`)), arr[i]);
+        }
     }
     
     getDataById(id, key){
@@ -46,16 +68,12 @@ class JSONProvider extends DataProviderBase{
         return this._fields[key][`${id}`];
     }
 
-    getUserById(id, key){
-        return this._users[key][`${id}`];
+    getUserById(id){
+        return this._users[`${id}`];
     }
 
     getFolderById(id, key){
         return this._structure[key][`${id}`];
-    }
-
-    getAll(){
-
     }
 }
 
